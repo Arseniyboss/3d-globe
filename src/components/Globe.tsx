c03d40526226acc4288ecaade0db9d3424d68104
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-05-27 22:05:00 +0200

    add hover state

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,9 +1,14 @@
 import { useTexture, Sphere } from '@react-three/drei'
+import { setCursor } from '@/utils'
 
 const Globe = () => {
   const texture = useTexture('/earth.jpg')
   return (
-    <Sphere scale={[1.8, 1.8, 1.8]}>
+    <Sphere
+      scale={[1.8, 1.8, 1.8]}
+      onPointerOver={() => setCursor('pointer')}
+      onPointerOut={() => setCursor('auto')}
+    >
       <meshStandardMaterial map={texture} />
     </Sphere>
   )
